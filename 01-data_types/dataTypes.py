0de5b5f4ac8d70a2e1c842ff7972e9e6db37b96e
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2016-05-04 22:48:54 -0300

    Day 01 - Data Types in Python :white_check_mark:

---
+++ 01-data_types/dataTypes.py
@@ -0,0 +1,19 @@
+import os
+
+firstInteger = 4
+firstDouble = 4.0
+firstString = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+secondInteger = 10
+secondDouble = 10.0
+secondString = 'Hakuna Matata'
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(firstInteger+secondInteger)
+# Print the sum of the double variables on a new line.
+print(firstDouble+secondDouble)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(firstString+secondString)
